e08c9d2b3e62f5f9a887898c3d78a1a3650f68e4
BrookeAL0012 <bbcecil17@gmail.com>
2022-04-07 12:42:05 -0500

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            if (a == 17 && b == 4)
+
+            { Console.WriteLine($"The area of a circle with a radius of {r} is {areaOfCircle}"); }
+        }
+
+    }
+}
